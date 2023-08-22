2a4b4eb2c18862939810543a925f064f80e3b752
cgrade <gradeisageek@gmail.com>
2023-08-22 23:31:20 +0100

    solution to task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A module that contains a type annotated func
+that takes in list of float an return their sum
+"""
+
+import typing
+
+
+def sum_list(input_list: list) -> float:
+    """A func that takes in list of float as param and return
+    float"""
+    sum: float = 0.0
+    for elem in input_list:
+        sum += elem
+    return sum
