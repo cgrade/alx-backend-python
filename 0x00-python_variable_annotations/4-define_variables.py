fc5036463724aef61a7b1011c40055aa1c73688f
cgrade <gradeisageek@gmail.com>
2023-08-22 22:48:48 +0100

    solution to task 4 added

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A module that contains defined variables that adhered to
+type annotation rules
+"""
+
+import typing
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
