e2d99f0113dd9ca0bb5e36795a31515fbd453434
cgrade <gradeisageek@gmail.com>
2023-08-23 00:32:01 +0100

    solution to task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+A module that contains a type annotated func `to-kv`
+which takes  param and return a tuple
+"""
+
+import typing
+
+
+def to_kv(k: str, v: int | float) -> typing.Tuple[str, int | float]:
+    """
+    a func that takes in `str: k` and `int | float: v` to return
+    A tuple (k, v)
+    """
+    return (k, v)
