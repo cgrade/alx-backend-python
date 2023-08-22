525afb82ba3011b9e6007b16e046c03e2aa24c44
cgrade <gradeisageek@gmail.com>
2023-08-22 05:02:46 +0100

    solution to task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A script that contains a type-annotated function """
+
+import typing
+
+
+def add(a: float, b: float) -> float:
+    """A function that take float as parame and returns float"""
+    return a + b
