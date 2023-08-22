aae52d28256845df197d82c07675ef29233d5c24
cgrade <gradeisageek@gmail.com>
2023-08-23 00:47:35 +0100

    solution to task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,7 +7,10 @@ which takes  param and return a tuple
 import typing
 
 
-def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, int | float]:
+tup = typing.Tuple[str, int | float]
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> tup:
     """
     a func that takes in `str: k` and `int | float: v` to return
     A tuple (k, v)
