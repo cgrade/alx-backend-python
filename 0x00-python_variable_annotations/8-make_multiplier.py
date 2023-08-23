b991930a7afb4dfe088f2cc0bde6afc99a186f92
cgrade <gradeisageek@gmail.com>
2023-08-23 01:27:43 +0100

    solution to task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+A module that contain a a complex type-annnotated
+func that takes in float param and returns a func.
+"""
+
+import typing
+
+
+cal = typing.Callable[float, float]
+
+
+def make_multiplier(multiplier: float) -> cal:
+    """
+    a func that takes in a float `param` and returns
+    a callable that multiply a float by `param`
+    """
+    def mul(x: float) -> float:
+        """
+        a func that takes 1 param `x: float` multiply it
+        by `multiplier: float` and returns it.
+        """
+        return x * multiplier
+    return mul 
