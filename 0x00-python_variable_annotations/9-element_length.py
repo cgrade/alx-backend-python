0f0f5dac2477d6f0ea6fe7e4769ce3d78e394bae
cgrade <gradeisageek@gmail.com>
2023-08-23 01:53:19 +0100

    solution to task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains variables that are corretly typed
+annotated
+"""
+
+import typing
+
+
+re = typing.List[typing.Tuple[str, int]]
+
+
+def element_length(lst: typing.List[typing.Union[str, int]]) -> re:
+    """
+    a func that take `lst: list` as param and returns each element of
+    the list and it's length
+    """
+    return [(i, len(i)) for i in lst]
