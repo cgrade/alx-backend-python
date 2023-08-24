024c73b5db52970a2701d2bc8278c6c4f070e426
cgrade <gradeisageek@gmail.com>
2023-08-24 14:51:04 +0100

    solution: to task 1-concurrent-couroutine

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+This module contain the solution to task 1: concurrent routines
+that describe how to run multiple coroutines at the same time
+it uses the asyncio, random and typing module to Answer this
+Assessment.
+"""
+
+import typing
+import asyncio
+import random as r
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """
+    A func that runs a coroutine "wait_random" n times within
+    max_delay amount of time, and returns the list of the delays
+    """
+    list_of_delays: typing.List[float] = []
+    for _ in range(n):
+        worker = asyncio.create_task(wait_random(max_delay))
+        rs = await worker
+        list_of_delays.append(rs)
+    return list_of_delays
